eb84bb0392651ccf310e3f0b14b015b2ad9a0c17
PrikrylovaJ <jprikrylova@email.cz>
2021-06-04 11:44:56 +0200

    Komponenta Vystavy - prvni navrh obsahu

--- src/Vystavy/index.jsx
+++ src/Vystavy/index.jsx
@@ -3,6 +3,26 @@ import './style.css';
 
 export const Vystavy = () => {
   return (
-    <h2>Výstavy</h2>
-  )
+    <section>
+      <h1>Výstavy</h1>
+      <div>
+        <div className="exhibition-container">
+          <div className="exhibiton-card">
+            <img className="exhibition-card__image" src="/assets/Adelheid Ka/Adelheidka2.jpg" height="200px" width="280px" alt=""/>
+            <a className="exhibition-card__author" href="adelheid.html">Adelheid Ka</a>
+            <p className="exhibition-card__title">Vernisáž</p>
+            <p className="exhibition-card__date">2. čer. - 30. čer.2021</p>
+            <p className="exhibition-card__place">Praha - Kulturní prostor Zdar</p>
+          </div>
+        </div>
+        <div class="exhibiton-card">
+          <img class="exhibition-card__image" src="assets/Pavel Vodnansky/Pavel Vodnansky1.jpeg" height="200px" width="280px" alt=""/>
+          <a class="exhibition-card__author" href="">Pavel Vodňanský</a>
+          <p class="exhibition-card__title">Vernisáž</p>
+          <p class="exhibition-card__date">2. čer. - 30. čer.2021</p>
+          <p class="exhibition-card__place">Praha - Kulturní prostor Zdar</p>
+        </div>
+      </div>
+    </section>
+  );
 };
