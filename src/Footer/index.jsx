19bfb486cc177fa7be6e813f15b25f2ec23aa9df
janavoborska <jana.dostalov@seznam.cz>
2021-06-05 18:40:15 +0200

    :stylování patika

--- src/Footer/index.jsx
+++ src/Footer/index.jsx
@@ -3,11 +3,15 @@ import './style.css';
 
 export const Footer = () => {
   return (
-    <div>
-      <p>Vinohradská 15</p>
-      <p>101 00 Praha 10</p>
-      <a href="mailto:hello@kpz.cz">hello@kpz.cz</a>
-      <p>tel: +420 739 622 622</p>
+    <div className="footer">
+      <div className="footer__email__phone">
+        <a href="mailto:hello@kpz.cz">hello@kpz.cz</a>
+        <p>tel: +420 739 622 622</p>
+      </div>
+      <div className="footer__address">
+        <p>U Vinohradského pivovaru 15</p>
+        <p>101 00 Praha 10</p>
+      </div>
     </div>
   );
 };
