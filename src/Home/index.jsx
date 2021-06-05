e5a008df8664eea51efff18ad1fe4e6caeffdea6
janavoborska <jana.dostalov@seznam.cz>
2021-06-05 09:41:30 +0200

    :opravena cesta k obrazkum

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import './style.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export const Uvodni = () => {
   return (
     <>
-       <main>
+      <main>
         <section className="homepage">
           <div className="homepage__container">
             <div className="homepage__content">
               <h1>KULTURNÍ PROSTOR ZDAR</h1>
-              <p className="homepage__text">Originální prostor pro umělecké aktivity všech kulturymilovných ve
-                  čtvrti, kde to žije! Pořádáme výstavy, kurzy, koncerty, přednášky.
-                  Nezbytností je útulná kavárna pro tvorbu i neformální setkání. Prostor
-                  je k dispozici i pro vaše akce.
+              <p className="homepage__text">
+                Originální prostor pro umělecké aktivity všech kulturymilovných
+                ve čtvrti, kde to žije! Pořádáme výstavy, kurzy, koncerty,
+                přednášky. Nezbytností je útulná kavárna pro tvorbu i neformální
+                setkání. Prostor je k dispozici i pro vaše akce.
               </p>
             </div>
             <div className="homepage__galerie">
@@ -21,21 +22,33 @@ export const Uvodni = () => {
                 <Link to="/vystavy">Výstavy</Link>
                 <img src="/assets/Ostatni/arrow.png" width="50px" alt="" />
               </div>
-              <img src="/assets/Adelhaid Ka/AdelheidKa3.jpg" width="50%" alt="" />
+              <img
+                src="/assets/Adelheid Ka/AdelheidKa3.jpg"
+                width="50%"
+                alt=""
+              />
               <div className="homepage_section1">
                 <h2>Pronájem prostor</h2>
                 <img src="/assets/Ostatní/arrow.png" width="50px" alt="" />
               </div>
-              <img src="/assets/Adelhaid Ka/AdelheidKa4.jpg" width="50%" alt="" />
+              <img
+                src="/assets/Adelheid Ka/AdelheidKa4.jpg"
+                width="50%"
+                alt=""
+              />
               <div className="homepage_section1">
                 <h2>Kurzy</h2>
                 <img src="/assets/Ostatni/arrow.png" width="50px" alt="" />
               </div>
-              <img src="/assets/Adelhaid Ka/AdelheidKa5.jpg" width="50%" alt="" />
+              <img
+                src="/assets/Adelheid Ka/AdelheidKa5.jpg"
+                width="50%"
+                alt=""
+              />
             </div>
           </div>
         </section>
       </main>
     </>
-  )
-}
+  );
+};
