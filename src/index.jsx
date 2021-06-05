f19da5477febdc7bb81b9b735f4a2e9b7b56aefd
PrikrylovaJ <jprikrylova@email.cz>
2021-06-05 19:15:13 +0200

    Komponenta Tickets

--- src/index.jsx
+++ src/index.jsx
@@ -13,6 +13,7 @@ import { Onas } from './O nas';
 import { Exhibitions } from './Exhibitions';
 import { ExhibitionDetail } from './ExhibitionDetail';
 import { Footer } from './Footer';
+import { Tickets } from './Tickets';
 import './style.css';
 
 const App = () => {
@@ -31,6 +32,9 @@ const App = () => {
         </Route>
         <Route path="/exhibitions" exact>
           <Exhibitions/>
+        </Route>
+        <Route path="/tickets" exact>
+          <Tickets/>
         </Route>
         <Route path="/exhibitions/:idExhibition" exact>
           <ExhibitionDetail/>
